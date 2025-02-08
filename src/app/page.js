4b0cb39b45097de00d7cfcc82497695cf3483d4f
Denys <denys@Denyss-MacBook-Air.local>
2025-02-07 22:47:31 -0800

    hello, Alex

---
+++ src/app/page.js
@@ -0,0 +1,160 @@
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+
+import { faUserNinja } from '@fortawesome/free-solid-svg-icons';
+
+
+export default function denis() {
+
+return(
+
+  
+     
+<div className='container'>
+
+
+<div className="message">
+    <div className="icon">
+        <FontAwesomeIcon icon={faUser} className="UserIcon"/>
+    </div>
+<div className='name-time'>
+<p className="name name-Denis">Denis</p>
+<p className="time">9:22pm</p>
+</div>
+    <div className="text">
+        <p className="Denis">hello everybody</p>
+    </div>
+</div>
+
+
+<div className="message">
+    <div className="icon">
+    <FontAwesomeIcon icon={faUserSecret} className='faUserSecret'/>
+    </div>
+<div className='name-time'>
+<p className="name name-Alex">Alex</p>
+<p className="time">9:23pm</p>
+</div>
+    <div className="text">
+        <p className="Alex">hello</p>
+    </div>
+</div>
+
+
+<div className="message">
+    <div className="icon">
+    <FontAwesomeIcon icon={faUserNinja} className='faUserNinja'/>
+    </div>
+<div className='name-time'>
+<p className="name name-Artem">Artem</p>
+<p className="time">9:24pm</p>
+</div>
+    <div className="text">
+        <p className="Artem">hi</p>
+    </div>
+</div>
+
+
+<div className="message">
+    <div className="icon">
+        <FontAwesomeIcon icon={faUser} className="UserIcon"/>
+    </div>
+<div className='name-time'>
+<p className="name name-Denis">Denis</p>
+<p className="time">9:25pm</p>
+</div>
+    <div className="text">
+        <p className="Denis">how are you doing guys?</p>
+    </div>
+</div>
+
+
+<div className="message">
+    <div className="icon">
+    <FontAwesomeIcon icon={faUserSecret} className='faUserSecret'/>
+    </div>
+<div className='name-time'>
+<p className="name name-Alex">Alex</p>
+<p className="time">9:26pm</p>
+</div>
+    <div className="text">
+        <p className="Alex">i'm good thank you</p>
+    </div>
+</div>
+
+
+<div className="message">
+    <div className="icon">
+    <FontAwesomeIcon icon={faUserNinja} className='faUserNinja'/>
+    </div>
+<div className='name-time'>
+<p className="name name-Artem">Artem</p>
+<p className="time">9:27pm</p>
+</div>
+    <div className="text">
+        <p className="Artem">me as well</p>
+    </div>
+</div>
+
+
+<div className="message">
+    <div className="icon">
+        <FontAwesomeIcon icon={faUser} className="UserIcon"/>
+    </div>
+<div className='name-time'>
+<p className="name name-Denis">Denis</p>
+<p className="time">9:28pm</p>
+</div>
+    <div className="text">
+        <p className="Denis">bye</p>     
+    </div>
+</div>
+
+
+<div className="message">
+    <div className="icon">
+    <FontAwesomeIcon icon={faUserSecret} className='faUserSecret'/>
+    </div>
+<div className='name-time'>
+<p className="name name-Alex">Alex</p>
+<p className="time">9:29pm</p>
+</div>
+    <div className="text">
+        <p className="Alex">good bye</p>   
+    </div>
+</div>
+
+
+<div className="message">
+    <div className="icon">
+    <FontAwesomeIcon icon={faUserNinja} className='faUserNinja'/>
+    </div>
+<div className='name-time'>
+<p className="name name-Artem">Artem</p>
+<p className="time">9:30pm</p>
+</div>
+    <div className="text">
+        <p className="Artem">see you soon</p>
+    </div>
+</div>
+
+    
+
+</div>
+
+
+)
+
+
+
+
+}
+
+
+
+
+
