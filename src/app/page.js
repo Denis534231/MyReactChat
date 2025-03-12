c23cfc898ec1bc9308c62f8d3ef283a89c416ded
Denys <githubdenis9@gmail.com>
2025-03-12 04:03:30 -0700

    put component into new file

--- src/app/page.js
+++ src/app/page.js
@@ -2,137 +2,32 @@
 
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-import { useState } from 'react';
-
-import { faUser } from "@fortawesome/free-solid-svg-icons";
-
-import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
-
-import { faUserNinja } from "@fortawesome/free-solid-svg-icons";
-
-import Message from "./components/MyMessages";
+import MyMainComponent from "./components/MainComponent";
 
 import Group from "./components/Group";
 
 export default function App() {
-
-  return(
-  <BrowserRouter>
-
-      <Link to='/'>Home</Link>
-      
-      <Link to='/group'>Group   </Link>  
-
-      <Routes>
-        <Route path="/" element={<MyMainComponent />} />
-        <Route path="/group" element={<Group />} />
-      </Routes>
-    
-  </BrowserRouter>
-  )
-}
-
-function MyMainComponent() {
-
-  const message_text = [
-
-    { id: 1, name: "Denis", time: "9:22", text: "hello everybody", icon: faUser },
-
-    { id: 2, name: "Alex", time: "9:23", text: "hello", icon: faUserSecret },
-
-    { id: 3, name: "Artem", time: "9:24", text: "hi", icon: faUserNinja },
-
-    { id: 4, name: "Denis", time: "9:25", text: "how are you doing guys?", icon: faUser },
-
-    { id: 5, name: "Alex", time: "9:26", text: "i'm good, thank you", icon: faUserSecret },
-
-    { id: 6, name: "Artem", time: "9:27", text: "me as well", icon: faUserNinja },
-
-    { id: 7, name: "Denis", time: "9:28", text: "good bye", icon: faUser },
-
-    { id: 8, name: "Alex", time: "9:29", text: "bye", icon: faUserSecret },
-
-    { id: 9, name: "Artem", time: "9:30", text: "see you soon", icon: faUserNinja },
-    
-  ]
-
- 
-  const click = () => {
-    
-    console.log(Messages)
-    console.log(Array.isArray(Messages))
-    
-  }
-
-  const newMessage = () => {
-
-    const timeNow = new Date().toLocaleTimeString()
-    setMessages([...Messages, {text: inputValue, name: SecondInputValue, time: timeNow }])
-    setInputValue('')
-
-  }
-  
-  
-
-  const change = (event) =>  {
-
-    setInputValue(event.target.value)
-    
-  }
-
-  const [Messages, setMessages] = useState([{name: "Denis", time: "9:22", text: "hello everybody", icon: faUser },
-
-    {name: "Alex", time: "9:23", text: "hello", icon: faUserSecret },
-
-    {name: "Artem", time: "9:24", text: "hi", icon: faUserNinja }])
-
-  const [inputValue, setInputValue] = useState('')
-
-  const [SecondInputValue, setSecondInputValue] = useState('')
-
-  
- 
   return (
-
-    
-
-
-    
-<div className="container">
-
-
-    
-      <div className="btn_ipt">
-
-        <input className="input" type="text" value={inputValue} onChange={change} placeholder="New message" />
-
-        <input className="input" type="text" value={SecondInputValue} onChange={(eventt) => {setSecondInputValue(eventt.target.value)}} placeholder="Name" />
-
-        <button className="button" onClick={newMessage}>Add</button>
-
-        <button className="button" onClick={click}>Checking</button>
-
-        <input className="input" type="text" placeholder="New group" />
-
-        <button className="button">Add</button>
-
-      </div>
-
-      {Messages.map(function (Iteration, index) {
-        return (
-         <div key={index}>
-            <Message name={Iteration.name} text={Iteration.text} icon={faUser} time={Iteration.time}/>
-         </div>
-        )
-      })}
-    </div>
-    
-  )
+    <>
+      <h1>Select</h1>
+      <BrowserRouter>
+        <ul>
+          <li>
+            <Link to="Chat">Chat </Link>
+          </li>
   
+          <li>
+            <Link to="Group"> Group </Link>{" "}
+          </li>
+        </ul>
+  
+        <Routes>
+          <Route path="Chat" element={<MyMainComponent />} />
+          <Route path="Group" element={<Group />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
-
-
-
-
-
+  
 
