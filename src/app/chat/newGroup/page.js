41bee66720c26d39c25706ffd1570bff36f1da9d
Denys <githubdenis9@gmail.com>
2025-05-15 19:31:56 -0700

    made an input and a button — when you press it, it goes to a URL like the text you wrote

--- src/app/chat/newGroup/page.js
+++ src/app/chat/newGroup/page.js
@@ -1,3 +1,24 @@
-export default function newGroup() {
-  return <h1>new group</h1>;
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function RouteInput() {
+  const [inputValue, setInputValue] = useState("");
+  const router = useRouter();
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleAddRoute = () => {
+    router.push(`/chat/pm/${inputValue}`);
+    setInputValue("");
+  };
+
+  return (
+    <div>
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+      <button onClick={handleAddRoute}>Add a new group</button>
+    </div>
+  );
 }
--- src/app/chat/pm/[id]/page.js
+++ src/app/chat/pm/[id]/page.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function myDynamicRoute({ params }) {
   return (
     <>
-      <h1>chat with: {params.id}</h1>
+      <h1>group with: {params.id}</h1>
     </>
   );
 }
